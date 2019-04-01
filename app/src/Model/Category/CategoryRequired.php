504b0f2cd78d63500f7cf9af01813a29ac8521b3
Vinicius Costa Castro <costacastrovinicius7@gmail.com>
2019-04-01 14:55:27 -0300

    fix(Star): Havia um erro no retorno de rotas ao verificar a questão

--- app/src/Model/Category/CategoryRequired.php
+++ app/src/Model/Category/CategoryRequired.php
@@ -7,17 +7,17 @@ use App\Mapper\Star;
 use App\Mapper\User;
 
 
-class CategoryRequired implements InterfaceCategory{
-
+class CategoryRequired implements InterfaceCategory {
     public function check($historyActivities,$star,$user){
+        if ($star->getCompleted())
+            return false;
         $count = 0;
         foreach($historyActivities as $history){
-            if($history->getActivity()->getCategory() == InterfaceCategory::REQUIRED){
+            if($history->getActivity()->getCategory() == InterfaceCategory::REQUIRED)
                 $count++;
-            }
         }
-        if($count >= 3 && !$star->getCompleted())
-            return true;       
+        if($count >= 3)
+            return true;
         return false;
     }
 
--- src/ts/pages/problems/script.ts
+++ src/ts/pages/problems/script.ts
@@ -59,11 +59,11 @@ class PluginProblems {
                         time = "Com o tempo de "+content.time+" s";
                     window.cosmo.dialog.success("Estrela "+content.star, "A resposta está correta e você ganhou a estrela de "+
                     content.star+". "+time, () => {
-                        window.location.href = window.base_url + window.cosmo.routes_name.activities_history
+                        window.location.href = window.base_url + window.history.back();
                     });
                 } else
                     window.cosmo.dialog.success("Meus Parabéns", "A resposta está correta", () => {
-                        window.location.href = window.base_url + window.cosmo.routes_name.activities_history
+                        window.location.href = window.base_url + window.history.back();
                     });
             }else
                 window.cosmo.dialog.error("Tente novamente!", content.message, () => {});
